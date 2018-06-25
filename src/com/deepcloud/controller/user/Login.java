0ba1d5483f43160f2d2c2cd533fd3744f24f0e7b
flying-pikachu <634588290@qq.com>
2018-06-25 14:30:29 +0800

    添加关于用户的操作

---
+++ src/com/deepcloud/controller/user/Login.java
@@ -0,0 +1,16 @@
+/**
+ * @ Author     ：xzp.
+ * @ Date       ：Created in 下午2:26 2018/6/25
+ * @ Description：${description}
+ * @ Modified By：
+ * @Version: $version$
+ */
+public class Login extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+}
