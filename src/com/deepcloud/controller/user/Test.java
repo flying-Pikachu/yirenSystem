fe3d4fa70c459659acbbd16ad7493ba252250fd6
flying-pikachu <634588290@qq.com>
2018-06-27 13:53:22 +0800

    更新了登录注册的前端判错付功能

---
+++ src/com/deepcloud/controller/user/Test.java
@@ -0,0 +1,17 @@
+/**
+ * @ Author     ：xzp.
+ * @ Date       ：Created in 下午1:29 2018/6/27
+ * @ Description：${description}
+ * @ Modified By：
+ * @Version: $version$
+ */
+@javax.servlet.annotation.WebServlet(name = "Test")
+public class Test extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+}
